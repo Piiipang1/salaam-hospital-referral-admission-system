ee399f92cbeae69a35733ff5cccca6fe91c621e6
Al-raafi C. Indanan <alraafiindanan50@gmail.com>
2026-06-23 13:58:18 +0800

    git commit -m "feat: add user API functions for fetching and updating user data" git push origin main

---
+++ frontend/src/api/users.api.js
@@ -0,0 +1,6 @@
+import api from './axios';
+export const getAllUsers    = () => api.get('/api/users').then(r => r.data);
+export const getUserById   = (id) => api.get(`/api/users/${id}`).then(r => r.data);
+export const createUser    = (data) => api.post('/api/users', data).then(r => r.data);
+export const updateUser    = (id, data) => api.put(`/api/users/${id}`, data).then(r => r.data);
+export const deactivateUser = (id) => api.delete(`/api/users/${id}`).then(r => r.data);
