3507008117090e93a69e7748bb72f178205e8eaa
Al-raafi C. Indanan <alraafiindanan50@gmail.com>
2026-06-23 13:57:43 +0800

    git commit -m "feat: add triages api" git push origin main

---
+++ frontend/src/api/triages.api.js
@@ -0,0 +1,6 @@
+import api from './axios';
+export const getAllTriages  = (params = {}) => api.get('/api/triages', { params }).then(r => r.data);
+export const getTriageById = (id) => api.get(`/api/triages/${id}`).then(r => r.data);
+export const createTriage  = (data) => api.post('/api/triages', data).then(r => r.data);
+export const updateTriage  = (id, data) => api.put(`/api/triages/${id}`, data).then(r => r.data);
+export const addVitalSigns = (id, data) => api.post(`/api/triages/${id}/vital-signs`, data).then(r => r.data);
